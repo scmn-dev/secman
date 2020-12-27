856d403d4246617969c439f7f64bfa29177bd378
abdfnx <abdfnabd@gmail.com>
2020-12-27 19:02:18 +0300

    fix install.sh error

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 # 4- some Linux platforms don't have git installed, so it's well checking is git command is exist
 
 UNAME=$(uname)
-smUrl=https://https://raw.githubusercontent.com/abdfnx/secman/main/secman
+smUrl=https://raw.githubusercontent.com/abdfnx/secman/main/secman
 smLocLD=/usr/local/bin
 
 successInstall() {
