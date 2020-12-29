21c3c3e1c258e3a7933aff2330132e4f1a8f4c43
abdfnx <abdfnabd@gmail.com>
2020-12-29 19:22:54 +0300

    rm files & move web to secman-site repo

--- web/base/apps.py
+++
@@ -1,4 +0,0 @@
-from django.apps import AppConfig
-
-class BaseConfig(AppConfig):
-    name = 'base'
--- web/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('blog/', views.blog, name="blog"),
-    path('docs/', views.docs, name="docs"),
-    path('doc/', views.doc, name="doc"),
-]
