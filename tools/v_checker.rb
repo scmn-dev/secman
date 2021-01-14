101c6703a48628eb1cd8db37ffb0c0587c05795f
abdfnx <abdfnabd@gmail.com>
2021-01-14 19:09:21 +0000

    test

--- tools/v_checker.rb
+++ tools/v_checker.rb
@@ -1,28 +1,16 @@
 require 'optparse'
-require 'colorize'
-
 $l = `verx abdfnx/secman -l`
-$c = `secman ver`
-
-def _n()
-    nr = "there's a new release of secman is avalaible:".yellow
-    ly = $l.cyan.bold
-    up = "to update it run".yellow
-    smu = "secman upd".blue
-    puts new_r = "#{nr} #{ly}#{up} #{smu}"
-end
+# $c = "./test"
 
 def check()
-    if "#{$l}" != "#{$c}"
-        _n
-    end
-        
-    if "#{$l}" == "#{$c}"
+    if $l == ""
+        # _n
         puts "same"
+    else
+        puts "not same"
     end
 end
 
-options = {}
 OptionParser.new do |opts|
   opts.on("-c", "--check", "check the version") do |c|
     check
