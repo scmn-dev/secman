cf9bd976c626b92ba11246b886f077280f8d0832
abdfnx <abdfnabd@gmail.com>
2021-01-15 05:53:27 +0000

    finally

--- tools/v_checker.rb
+++ tools/v_checker.rb
@@ -1,13 +1,20 @@
 require 'optparse'
+require 'colorize'
+
 $l = `verx abdfnx/secman -l`
 $c = `secman verx`
 
+def _n()
+    ly = $l.cyan.bold
+    nr = "there's a new release of secman is avalaible:".yellow
+    up = "to update it run".yellow
+    smu = "secman upd".blue
+    puts new_r = "#{nr} #{ly}#{up} #{smu}"
+end
+
 def check()
     if $l != $c
-        # _n
-        puts "not same"
-    else
-        puts "same"
+        _n
     end
 end
 
