f35350de5f267d43f19425cc922de777c635aedf
abdfnx <abdfnabd@gmail.com>
2021-01-23 19:15:05 +0300

    some M

--- tools/v_checker.rb
+++ tools/v_checker.rb
@@ -6,7 +6,7 @@ $c = `secman verx`
 
 def _n()
   ly = $l.cyan.bold
-  nr = "there's a new release of secman is avalaible: #{$l}".yellow
+  nr = "there's a new release of secman is avalaible:".yellow
   up = "to update it run".yellow
   smu = "secman upd".blue
   puts new_r = "#{nr} #{ly}#{up} #{smu}"
