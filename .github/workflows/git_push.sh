31ce6a3c44c4b836f5273182c07969c99c67055b
abdfnx <abdfnabd@gmail.com>
2021-01-25 21:30:02 +0300

    up

--- .github/workflows/git_push.sh
+++ .github/workflows/git_push.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 ### Set git users to the commit we are building from ###
-git config user.name "$(git --no-pager log --format=format:'%an' -n 1)"
-git config user.email "$(git --no-pager log --format=format:'%ae' -n 1)"
-
-### core ###
-cd ~/build
-git init
-git add .
-git commit -m "secman mac build"
-git branch -M main
-git remote add origin https://github.com/secman-team/macosx
-git push -u origin main
+sh -c "git config --global user.name '${GITHUB_ACTOR}' \
+    && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com' \
+    && cd ~/build \
+    && git init && \
+    && git add . \
+    && git commit -m \"secman mac build\" \
+    && git branch -M main \
+    && git remote add origin https://github.com/secman-team/macosx \
+    && git push -u origin main"
