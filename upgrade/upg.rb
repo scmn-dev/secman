1d8df4ce4cf487f88d15e5f01d60fd975a86439d
abdfnx <abdfnabd@gmail.com>
2021-03-04 17:16:14 +0300

    update tools, fix upgrade/upg.rb

--- upgrade/upg.rb
+++ upgrade/upg.rb
@@ -4,10 +4,17 @@ require "colorize"
 $l = `verx secman-team/secman -l`
 $c = `secman verx`
 
-smLoc = "/usr/local/bin"
+$smLoc = "/usr/local/bin"
 
-def _os
-    @_os ||= (
+def pre_upgrade
+    system("sudo rm -rf #{$smLoc}/secman*")
+    system("sudo rm -rf #{$smLoc}/cgit*")
+    system("sudo rm -rf #{$smLoc}/verx*")
+    system("sudo rm -rf /home/sm")
+end
+
+def os
+    @os ||= (
         host_os = RbConfig::CONFIG['host_os']
         shared_gh_url = "https://raw.githubusercontent.com/secman-team/install/HEAD/install"
         lin = "linux".yellow
@@ -16,40 +23,28 @@ def _os
         case host_os
         when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
             :windows
-            puts "secman upd command is only supported for #{lin} and #{osx}"
+            puts "secman upg command is only supported for #{lin} and #{osx}"
         when /darwin|mac os/
             :macosx
-            system("$(curl -fsSL #{shared_gh_url}_osx.sh) | bash")
+            system("curl -fsSL #{shared_gh_url}_osx.sh | bash")
         when /linux/
             :linux
-            system("$(curl -fsSL #{shared_gh_url}_linux.sh) | bash")
+            system("curl -fsSL #{shared_gh_url}_linux.sh | bash")
         else
             raise Error::WebDriverError, "unknown os: #{host_os.inspect}"
         end
     )
 end
 
-sm = "secman".blue
+sm = "secman".cyan
 
 if $l == $c
     al = "is already up-to-date and it's the latest release"
     puts "#{sm} #{al} #{$l.yellow}"
     
 elsif $l != $c
-    @os ||= (
-        host_os = RbConfig::CONFIG['host_os']
-
-        case host_os
-        when /darwin|mac os|linux/
-            :macosx_linux
-            system("sudo rm -rf #{smLoc}/secman*")
-            system("sudo rm -rf #{smLoc}/cgit*")
-            system("sudo rm -rf #{smLoc}/verx*")
-            _os()
-        else
-            raise Error::WebDriverError, "unknown os: #{host_os.inspect}"
-        end
-    )
+    pre_upgrade()
+    os()
 
     puts "#{sm} was upgraded successfully"
 end
