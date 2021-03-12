53842f3b1a7cfd52727cf184b88912f843b84a9e
abdfnx <abdfnabd@gmail.com>
2021-03-12 22:12:39 +0300

    fix version checker in core.go

--- core/core.go
+++ core/core.go
@@ -262,12 +262,6 @@ func main() {
 			_cmd secman-un "sudo wget -P /usr/local/bin https://raw.githubusercontent.com/secman-team/secman/HEAD/packages/secman-un"
 
 			_cmd secman-sync "sudo wget -P /usr/local/bin https://raw.githubusercontent.com/secman-team/secman/HEAD/api/sync/secman-sync"
-			
-			if [ -d /home/sm ] && [ -x "$(command -v verx)" ] && [ -x "$(command -v cgit)" ] && [ -x "$(command -v secman-un)" ] && [ -x "$(command -v secman-sync)" ]; then
-				echo "secman was fixed successfully 👍"
-			fi
-
-			echo "run secman again after fix"
 		`
 
 	wCheck :=
@@ -277,12 +271,9 @@ func main() {
 			if(!(Test-Path -path $directoyPath)) {
 				echo "installing sm..."
 				git clone https://github.com/secman-team/sm-win $directoyPath
+				echo "installing ruby deps..."
+				gem install colorize optparse
 			}
-
-			echo "installing ruby deps..."
-			gem install colorize optparse
-
-			echo "run secman again after fix"
 		`	
 
 	if runtime.GOOS == "windows" {
