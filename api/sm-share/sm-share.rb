89a7f07684cb14818e440ab16119983541c6a0b8
Timothee <80710181+Timothee-Cardoso@users.noreply.github.com>
2021-03-24 18:53:01 +0300

    create sm-share subprogram

---
+++ api/sm-share/sm-share.rb
@@ -0,0 +1,15 @@
+# how sm-share should work
+
+"""
+requirements
+~/.secman in sync mod
+
+form of sm-share
+to share a specific password
+  $ sm-share -p $PASSWORDNAME | $FOLDER/PASSWORDNAME
+    Enter the Github username to the user that you want to share it with him:
+    
+to share all ~/.secman
+  $ sm-share -a
+    Enter the Github username to the user that you want to share all ~/.secman with him:
+"""
