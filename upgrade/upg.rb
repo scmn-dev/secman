374f3d403233e5c491dbd66729420a42166faf94
abdfnx <abdfnabd@gmail.com>
2021-03-27 13:48:50 +0300

    short upg.rb

--- upgrade/upg.rb
+++ upgrade/upg.rb
@@ -13,24 +13,8 @@ def pre_upgrade
     system("sudo rm -rf /home/sm")
 end
 
-def os
-    @os ||= (
-        host_os = RbConfig::CONFIG['host_os']
-        lin = "linux".yellow
-        osx = "osx".black
-
-        case host_os
-        when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-            :windows
-            puts "secman upg command is only supported for #{lin} and #{osx}"
-
-        when /linux|darwin|mac os/
-            :linux_macos
-            system("curl -fsSL https://secman-team.github.io/install/install.sh | bash")
-        else
-            raise Error::WebDriverError, "unknown os: #{host_os.inspect}"
-        end
-    )
+def core
+    system("curl -fsSL https://secman-team.github.io/install/install.sh | bash")
 end
 
 sm = "secman".cyan
@@ -41,7 +25,7 @@ if $l == $c
     
 elsif $l != $c
     pre_upgrade()
-    os()
+    core()
 
     puts "#{sm} was upgraded successfully"
 end
