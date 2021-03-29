8c18842b8edc33f98dc5fd6e6ea716dd8bd17e58
abdfnx <abdfnabd@gmail.com>
2021-03-29 19:52:48 +0300

    rm docs

--- docs/commands/clean.md
+++
@@ -1,7 +0,0 @@
-# Clean your ~/.secman (delete it).
-
-Usage:
-  `secman clean [flags]`
-
-Flags:
-  **-h**, **--help**   help for clean
--- docs/map.js
+++
@@ -1,4 +0,0 @@
-const decoder = new TextDecoder("utf-8");
-const data = await Deno.readFile("map.json");
-
-console.log(decoder.decode(data));
