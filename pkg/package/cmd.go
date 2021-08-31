5ddbb1cdc3dce67d880069f0e723bfcbc21e1e59
abdfnx <abdfnabd@gmail.com>
2021-08-31 19:41:09 +0300

    add `package list` command

--- pkg/package/cmd.go
+++ pkg/package/cmd.go
@@ -12,6 +12,9 @@ import (
 )
 
 func PackageCmd(f *cmdutil.Factory) *cobra.Command {
+	m := f.PackageManager
+	io := f.IOStreams
+
 	cmd := cobra.Command{
 		Use:   "package",
 		Short: "Manage secman package",
@@ -20,6 +23,44 @@ func PackageCmd(f *cmdutil.Factory) *cobra.Command {
 		`, "`"),
 		Aliases: []string{"pkg", "packages"},
 	}
+
+	cmd.AddCommand(
+		&cobra.Command{
+			Use:   "list",
+			Short: "List installed packages",
+			Args:  cobra.NoArgs,
+			Aliases: []string{"ls"},
+			RunE: func(cmd *cobra.Command, args []string) error {
+				cmds := m.List(true)
+				if len(cmds) == 0 {
+					return errors.New("no packages installed")
+				}
+
+				cs := io.ColorScheme()
+				t := utils.NewTablePrinter(io)
+				for _, c := range cmds {
+					var repo string
+					if u, err := git.ParseURL(c.URL()); err == nil {
+						if r, err := ghrepo.FromURL(u); err == nil {
+							repo = ghrepo.FullName(r)
+						}
+					}
+
+					t.AddField(fmt.Sprintf("sm %s", c.Name()), nil, nil)
+					t.AddField(repo, nil, nil)
+					var updateAvailable string
+					if c.UpdateAvailable() {
+						updateAvailable = "Upgrade available"
+					}
+
+					t.AddField(updateAvailable, nil, cs.Green)
+					t.EndRow()
+				}
+
+				return t.Render()
+			},
+		},
+	)
 
 	return &cmd
 }
