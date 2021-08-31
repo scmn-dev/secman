c0eaf1a3e7a8266b036bb57ad60b462c4ac84f78
abdfnx <abdfnabd@gmail.com>
2021-08-31 19:06:25 +0300

    add `package` command with `pkg` & `packages` aliases

--- pkg/package/cmd.go
+++ pkg/package/cmd.go
@@ -8,4 +8,18 @@ import (
 
 	"github.com/MakeNowJust/heredoc"
 	"github.com/spf13/cobra"
+	"github.com/scmn-dev/gh-api/pkg/cmdutil"
 )
+
+func PackageCmd(f *cmdutil.Factory) *cobra.Command {
+	cmd := cobra.Command{
+		Use:   "package",
+		Short: "Manage secman package",
+		Long: heredoc.Docf(`
+			Secman Package are repositories that provide more additional apps.
+		`, "`"),
+		Aliases: []string{"pkg", "packages"},
+	}
+
+	return &cmd
+}
