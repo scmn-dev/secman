76a9f2529025428c9325cafa51d9d0b03bd88f37
abdfnx <abdfnabd@gmail.com>
2021-08-31 13:24:52 +0300

    add validation functions

--- cluster/cluster_type.go
+++ cluster/cluster_type.go
@@ -1,5 +1,9 @@
 package cluster
 
+import (
+	"fmt"
+)
+
 type Cluster interface {
 	Get(string, string) (string, error)
 	GetWithSource(string, string) (string, string, error)
@@ -46,4 +50,45 @@ var clusterOptions = []ClusterOption{
 
 func ClusterOptions() []ClusterOption {
 	return clusterOptions
+}
+
+func ValidateKey(key string) error {
+	for _, configKey := range clusterOptions {
+		if key == configKey.Key {
+			return nil
+		}
+	}
+
+	return fmt.Errorf("invalid key")
+}
+
+type InvalidValueError struct {
+	ValidValues []string
+}
+
+func (e InvalidValueError) Error() string {
+	return "invalid value"
+}
+
+func ValidateValue(key, value string) error {
+	var validValues []string
+
+	for _, v := range clusterOptions {
+		if v.Key == key {
+			validValues = v.AllowedValues
+			break
+		}
+	}
+
+	if validValues == nil {
+		return nil
+	}
+
+	for _, v := range validValues {
+		if v == value {
+			return nil
+		}
+	}
+
+	return &InvalidValueError{ValidValues: validValues}
 }
