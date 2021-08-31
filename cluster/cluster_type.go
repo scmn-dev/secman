d9ff5e60cbf62875a3cda74ac127f5dc658b9a53
abdfnx <abdfnabd@gmail.com>
2021-08-31 13:23:13 +0300

    create `clusterOptions` & `ClusterOptions` function

--- cluster/cluster_type.go
+++ cluster/cluster_type.go
@@ -18,3 +18,32 @@ type ClusterOption struct {
 	DefaultValue  string
 	AllowedValues []string
 }
+
+var clusterOptions = []ClusterOption{
+	{
+		Key:           "git_protocol",
+		Description:   "the protocol to use for git clone and push operations",
+		DefaultValue:  "https",
+		AllowedValues: []string{"https", "ssh"},
+	},
+	{
+		Key:           "prompt",
+		Description:   "toggle interactive prompting in the terminal",
+		DefaultValue:  "enabled",
+		AllowedValues: []string{"enabled", "disabled"},
+	},
+	{
+		Key:          "http_unix_socket",
+		Description:  "the path to a unix socket through which to make HTTP connection",
+		DefaultValue: "",
+	},
+	{
+		Key:          "browser",
+		Description:  "the web browser to use for opening URLs",
+		DefaultValue: "",
+	},
+}
+
+func ClusterOptions() []ClusterOption {
+	return clusterOptions
+}
