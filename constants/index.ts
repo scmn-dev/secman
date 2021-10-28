8d1d8ff67085ebc24268d8194d8033d37e91470b
abdfnx <abdfnabd@gmail.com>
2021-10-28 22:36:08 +0300

    add secman cli constants

---
+++ constants/index.ts
@@ -0,0 +1,75 @@
+import { platform } from "os";
+import env from "dotenv";
+
+env.config();
+
+export const PRIMARY_COLOR: any = "#1163E6";
+export const SENDGRID_API_KEY: any = process.env.SENDGRID_API_KEY;
+export const API_URL: any = "https://api.secman.dev";
+export const CHARS = {
+  alphabet: "abcdefghijklmnopqrstuvwxyz",
+  numeric: "0123456789",
+  special: "_-+=)/(*&^%$#@%!?~",
+};
+export const DOT_SECMAN_PATH = ".secman";
+export const SECMAN_CONFIG_PATH: any = `/${DOT_SECMAN_PATH}/config.json`;
+export const SECMAN_DATA_PATH: any = `/${DOT_SECMAN_PATH}/data.json`;
+export const SECMAN_SETTINGS_PATH: any = `/${DOT_SECMAN_PATH}/settings.json`;
+export const SECMAN_SETTINGS_URL: any =
+  "https://raw.githubusercontent.com/scmn-dev/secman/main/contents/data/settings.json";
+export const SECMAN_EDITOR_PATH: any =
+  platform() === "win32"
+    ? `/${DOT_SECMAN_PATH}/editor.exe`
+    : `/${DOT_SECMAN_PATH}/editor`;
+export let COMPLEXIES = [
+  { name: "abc", value: CHARS.alphabet, checked: true, visible: false },
+  { name: "Numbers", value: CHARS.numeric, checked: true },
+  { name: "Symbols", value: CHARS.special, checked: false },
+  {
+    name: "Capital Letters",
+    value: CHARS.alphabet.toUpperCase(),
+    checked: true,
+  },
+];
+export const LOGINS_ENCRYPTED_FIELDS = ["username", "password", "extra"];
+export const CCS_ENCRYPTED_FIELDS = [
+  "type",
+  "number",
+  "expiry_date",
+  "cardholder_name",
+  "verification_number",
+];
+export const EMAILS_ENCRYPTED_FIELDS = ["email", "password"];
+export const NOTES_ENCRYPTED_FIELDS = ["note"];
+export const SERVERS_ENCRYPTED_FIELDS = [
+  "ip",
+  "username",
+  "password",
+  "hosting_username",
+  "hosting_password",
+  "admin_username",
+  "admin_password",
+  "extra",
+];
+export const TABLE_DESIGN = {
+  border: {
+    topBody: `─`,
+    topJoin: `┬`,
+    topLeft: `╭`,
+    topRight: `╮`,
+
+    bottomBody: `─`,
+    bottomJoin: `┴`,
+    bottomLeft: `╰`,
+    bottomRight: `╯`,
+
+    bodyLeft: `│`,
+    bodyRight: `│`,
+    bodyJoin: `│`,
+
+    joinBody: `─`,
+    joinLeft: `├`,
+    joinRight: `┤`,
+    joinJoin: `┼`,
+  },
+};
