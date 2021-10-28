97f40c00d11640fbc64354d90e2121c441dc8535
abdfnx <abdfnabd@gmail.com>
2021-10-28 22:51:36 +0300

    create secman documentation command

---
+++ src/commands/docs.ts
@@ -0,0 +1,23 @@
+import { Command, flags } from "@oclif/command";
+import { spnr as spinner } from "@secman/spinner";
+import { cli } from "cli-ux";
+
+export default class Docs extends Command {
+  static description = "Open Secman Documentation in default browser.";
+
+  static aliases = ["doc"];
+
+  static flags = {
+    help: flags.help({ char: "h" }),
+  };
+
+  async run() {
+    const { flags } = this.parse(Docs);
+
+    const opening = spinner("📚 Opening docs...").start();
+
+    await cli.open("https://secman.dev/docs");
+
+    opening.stop();
+  }
+}
