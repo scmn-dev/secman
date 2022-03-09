cfcaf56662d67e7609c3b64b13efaf0bd16daf94
abdfnx <abdfnabd@gmail.com>
2022-03-09 15:34:35 +0300

    start building secman commands in `app` pkg, add new modules

---
+++ app/opts.go
@@ -0,0 +1,24 @@
+package app
+
+import "github.com/scmn-dev/secman/pkg/options"
+
+var PwOpts = options.PasswordsOptions{
+	Password:     "",
+	Logins: 	  false,
+	CreditCards:  false,
+	Emails: 	  false,
+	Notes: 		  false,
+	Servers: 	  false,
+	ShowHidden:   false,
+	ShowJsonView: false,
+	ShowTreeView: false,
+}
+
+var GenOpts = options.GenOptions{
+	Length: 10,
+	Raw:    false,
+}
+
+var AuthOpts = options.AuthOptions{
+	ConfirmLogout: false,
+}
--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
 	github.com/muesli/reflow v0.3.0
 	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739
+	github.com/scmn-dev/browser v0.1.3
 	github.com/spf13/cobra v1.3.0
 	github.com/spf13/viper v1.10.1
 	github.com/tidwall/gjson v1.14.0
